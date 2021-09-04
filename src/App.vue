db0ba8ceac4915c3cc14fb097250debce357cd21
ZiyodulloRakhmonberdiev <ziyodillo808@gmail.com>
2021-09-04 09:24:14 +0500

    bismillah

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <main>
+      <div class="navigation">
+        <header>
+          <h1>Samar Badriddinov</h1>
+        </header>
+        <nav>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          <a href="#">Portfolio</a>
+        </nav>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
   components: {
-    HelloWorld,
+
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body{
+    font-family: 'monospace'. sans-serif;
+  }
+  main{
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    overflow: hidden;
+  }
+  .navigation{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 10vh;
+    width: 100%;
+    align-items: center;
+  }
+  header h1{
+    color: #fff;
+    font-size: 32px;
+  }
 </style>
